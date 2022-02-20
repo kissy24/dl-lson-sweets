84b2972884412ff74c1fd1256979d589a693cf6d
kissy24 <kissy105808j@gmail.com>
2022-02-20 15:18:51 +0900

    create production code.

---
+++ lsondessert.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Image struct {
+	URL      string
+	FileName string
+}
+
+func GetImageURLs(domainUrl string, srcDirUrl string, imgDirUrl string) ([]Image, error) {
+	doc, err := goquery.NewDocument(domainUrl + srcDirUrl)
+	if err != nil {
+		return nil, err
+	}
+	var urls []Image
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		imgFileUrl, _ := s.Attr("src")
+		imgFileName, _ := s.Attr("alt")
+		hasImgPattern := strings.Contains(imgFileUrl, imgDirUrl)
+		if hasImgPattern {
+			imgUrl := domainUrl + imgFileUrl
+			img := Image{imgUrl, imgFileName}
+			urls = append(urls, img)
+		}
+	})
+	return urls, nil
+}
+
+func DownloadImages(imgs []Image, imgDir string) {
+	for _, img := range imgs {
+		response, err := http.Get(img.URL)
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+		if _, err := os.Stat(imgDir); os.IsNotExist(err) {
+			os.MkdirAll(imgDir, 0777)
+		}
+		file, err := os.Create(imgDir + "/" + img.FileName + ".jpg")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		io.Copy(file, response.Body)
+	}
+}
+
+func main() {
+	domainUrl := "https://www.lawson.co.jp"
+	srcDirUrl := "/recommend/original/dessert/"
+	imgDirUrl := "/recommend/original/detail/img/"
+	imgUrls, err := GetImageURLs(domainUrl, srcDirUrl, imgDirUrl)
+	if err != nil {
+		panic(err)
+	}
+	dateDir := time.Now().Format("2006-01-02")
+	DownloadImages(imgUrls, "./images/"+dateDir)
+}
